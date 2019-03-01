903b01a6f2667681d3bc5cf3a35f7416976dc75d
Pierre Luc Nichols <pierrelucnichols@gmail.com>
2019-03-01 08:57:45 +0000

    Update Footer copyright year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <Box mt={80} mb={20}>
       <FooterCopyrightStyled>
-        &copy; 2018 Bionic Beetle Ltd &bull; Registered in England and Wales, no 09268216
+        &copy; 2019 Bionic Beetle Ltd &bull; Registered in England and Wales, no 09268216
       </FooterCopyrightStyled>
     </Box>
   );
